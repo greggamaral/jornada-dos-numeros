8274a75a2c20ff151ad55213fc616e6dd47d8199
Greg <greghory.a@outlook.com>
2026-03-28 16:17:10 -0300

    refactor: unifica LevelManager e LevelManager2 em um unico arquivo
    Os dois scripts eram 95% identicos, diferindo apenas em pitch, musicas
    de audio e cenas de navegacao. Substituidos por um unico LevelManager.gd
    com @export var level_index que controla o comportamento de cada fase.
    
    level2.tscn atualizado para usar LevelManager.gd com level_index = 2.

--- scenes/levels/fase_animais/LevelManager.gd
+++ scenes/levels/fase_animais/LevelManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var level_index: int = 1
+
 @onready var camera_2d = $"../SceneObjects/Camera2D"
 @onready var solve = $"../UI/Solve"
 @onready var texto_1 = $"../UI/Dialogue/texto_1"
@@ -10,7 +12,7 @@ extends Node
 
 var counter #counts wrong answers
 var result
-var pitch = 2.0 #tts config
+var pitch: float
 var speed = 1.3 #tts config
 var volume = AudioController.volume #tts config
 var speaker: String = DisplayServer.tts_get_voices_for_language("pt_BR")[0] #tts config
@@ -22,14 +24,22 @@ var y_position #vertical camera position
 func _ready():
 	x_position = -1
 	cur_position = -1
-	y_margin =  0.25
+	y_margin = 0.25
 	y_position = -0.4
 	counter = 0
-	result = dialogue.result 
-	AutoloadScene.previous_scene = "res://scenes/levels/fase_animais/level1.tscn"
+	result = dialogue.result
+
+	if level_index == 1:
+		pitch = 2.0
+		AutoloadScene.previous_scene = "res://scenes/levels/fase_animais/level1.tscn"
+		AudioController._play_backmusic()
+	else:
+		pitch = 1.0
+		AutoloadScene.previous_scene = "res://scenes/levels/fase_animais/level2.tscn"
+		AudioController._play_backmusic2()
+
 	DisplayServer.tts_resume()
-	AudioController._play_backmusic()
-	
+
 func _process(delta):
 	#input button action
 	if Input.is_action_just_pressed("pause"):
@@ -37,21 +47,21 @@ func _process(delta):
 		AudioController._play_select()
 		AudioController._pause_backmusic(true)
 		get_tree().change_scene_to_file("res://scenes/menu/menu_pausa.tscn")
-		
+
 	if Input.is_action_just_pressed("left"):
 		x_position -= 1
 		if x_position < 0:
 			x_position = -1
 		cur_position = x_position
 		DisplayServer.tts_stop()
-		
+
 	if Input.is_action_just_pressed("right"):
 		x_position += 1
 		if x_position > 1:
 			x_position = 1
 		cur_position = x_position
 		DisplayServer.tts_stop()
-		
+
 	if Input.is_action_just_pressed("up"):
 		x_position = 0
 		y_position = -1.0
@@ -59,13 +69,13 @@ func _process(delta):
 		solve.grab_focus()
 		AudioController._play_question()
 		DisplayServer.tts_stop()
-		
+
 	if Input.is_action_just_pressed("down"):
 		x_position = cur_position
 		y_position = -0.4
 		y_margin = 0.25
 		DisplayServer.tts_stop()
-	
+
 	#Dragging camera
 	if y_position == -0.4:
 		camera_2d.set_drag_horizontal_offset(x_position)
@@ -73,7 +83,7 @@ func _process(delta):
 		camera_2d.set_drag_horizontal_offset(0)
 	camera_2d.set_drag_vertical_offset(y_position)
 	camera_2d.set_drag_margin(SIDE_BOTTOM, y_margin)
-	
+
 	#showing/hiding texts and audiodescription according camera position
 	if x_position == -1.0 and y_position == -0.4:
 		texto_1.show()
@@ -81,22 +91,22 @@ func _process(delta):
 		_speak(text1)
 	else:
 		texto_1.hide()
-		
+
 	if x_position == 0 and y_position == -0.4:
 		texto_2.show()
 		var text2: String = $"../UI/Dialogue/texto_2/Label".get("text")
 		_speak(text2)
 	else:
 		texto_2.hide()
-		
+
 	if x_position == 1.0 and y_position == -0.4:
 		texto_3.show()
 		var text3: String = $"../UI/Dialogue/texto_3/Label".get("text")
 		_speak(text3)
 	else:
 		texto_3.hide()
-		
-	if  y_position == -1.0: 
+
+	if y_position == -1.0:
 		texto_4.show()
 	else:
 		texto_4.hide()
@@ -105,17 +115,25 @@ func _process(delta):
 func _speak(text):
 	DisplayServer.tts_speak(text, speaker, volume, pitch, speed, 2)
 	DisplayServer.tts_pause()
-	
+
 #sets effects after submitting text in LineEdit
 func _on_solve_text_submitted(new_text):
 	if new_text == str(result):
-		AudioController._play_congrats()
-		AutoloadScene.previous_scene = "res://scenes/levels/fase_animais/level2.tscn"
-		AudioController._play_backmusic()
+		if level_index == 1:
+			AudioController._play_congrats()
+			AutoloadScene.previous_scene = "res://scenes/levels/fase_animais/level2.tscn"
+			AudioController._play_backmusic()
+		else:
+			AudioController._play_congrats2()
+			AutoloadScene.previous_scene = "res://scenes/level_selection/level_selection.tscn"
+			AudioController._pause_backmusic(true)
 	else:
 		counter += 1
 		if counter > 2:
-			AudioController._play_answer()
+			if level_index == 1:
+				AudioController._play_answer()
+			else:
+				AudioController._play_answer2()
 		else:
 			AudioController._play_try()
 	solve.text = ""
