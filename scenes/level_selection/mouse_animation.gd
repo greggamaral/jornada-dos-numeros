21e1a93d5f9a8a115451d31ee6711fe591111a4c
Greg <greghory.a@outlook.com>
2026-03-28 16:08:05 -0300

    style: remove comentarios de template gerados pelo editor
    Remove os comentarios 'Replace with function body.' deixados pelo
    template de script do Godot em mouse_animation.gd e MenuPausa.gd

--- scenes/level_selection/mouse_animation.gd
+++ scenes/level_selection/mouse_animation.gd
@@ -1,23 +1,17 @@
 extends Area2D
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass# Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_mouse_entered():
-	$"../../AnimationPlayer".play("new_animation") # Replace with function body. # Replace with function body.
-
+	$"../../AnimationPlayer".play("new_animation")
 
 func _on_mouse_exited():
-	$"../../AnimationPlayer".play("RESET") # Replace with function body.# Replace with function body.
-
+	$"../../AnimationPlayer".play("RESET")
 
 func _on_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed:
-		get_tree().change_scene_to_file("res://node_2d.tscn") # Replace with function body.
+		get_tree().change_scene_to_file("res://node_2d.tscn")
--- scenes/menu/MenuPausa.gd
+++ scenes/menu/MenuPausa.gd
@@ -12,42 +12,41 @@ var volume = 70
 func _ready():
 	DisplayServer.tts_stop()
 	continuar.grab_focus()
-	
+
 func _process(delta):
 	pass
-	
-	
+
+
 func _on_continuar_pressed():
 	DisplayServer.tts_stop()
 	get_tree().change_scene_to_file(AutoloadScene.previous_scene)
 	AudioController._play_exit()
 	AudioController._pause_backmusic(false)
-	
-	
+
+
 func _on_audio_pressed():
-	#DisplayServer.tts_stop() # Replace with function body.
 	AudioController._play_select()
 	DisplayServer.tts_stop()
 	get_tree().change_scene_to_file("res://scenes/menu/audio_settings.tscn")
 
 func _on_sair_pressed():
-	DisplayServer.tts_stop() # Replace with function body.
+	DisplayServer.tts_stop()
 	AudioController._play_select()
 	AutoloadScene.previous_scene == "res://scenes/level_selection/level_selection.tscn"
 	get_tree().change_scene_to_file("res://scenes/level_selection/level_selection.tscn")
 
 func _on_audio_focus_entered():
 	DisplayServer.tts_speak("Áudio", speaker, AudioController.volume, 1.0, speed, 1)
-	
-	
+
+
 func _on_continuar_focus_entered():
 	DisplayServer.tts_speak("Continuar", speaker, AudioController.volume, 1.0, speed, 1)
-	
-	
+
+
 func _on_sair_focus_entered():
 	DisplayServer.tts_speak("Sair", speaker, AudioController.volume, 1.0, speed, 1)
-	
-	
+
+
 func _on_continuar_focus_exited():
 	if  Input.is_action_just_pressed("up") or  Input.is_action_just_pressed("down"):
 		AudioController._play_cursor()
